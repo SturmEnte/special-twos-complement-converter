b53b5817b70f6e1be7369f0fc74442ff00a22791
Jonas <sturmente@proton.me>
2024-05-06 18:10:58 +0200

    Finished converter

--- specialTwosComplementConverter/Program.cs
+++ specialTwosComplementConverter/Program.cs
@@ -5,7 +5,7 @@ byte byte1 = Convert.ToByte("10110101", 2);
 byte byte2 = Convert.ToByte("10000000", 2);
 
 // Testing code
-if(convertSpecialTwosComplement(byte1, byte2) == expectedResult)
+if(ConvertSpecialTwosComplement(byte1, byte2) == expectedResult)
 {
     Console.WriteLine("Correct result");
 } else
@@ -17,7 +17,53 @@ if(convertSpecialTwosComplement(byte1, byte2) == expectedResult)
 // byte 1 -128 64 32 16 8 4 2 1
 // byte 2 0.5 x x x x x x x
 // The bytes are read from a LM75 module 
-double convertSpecialTwosComplement(byte byte1, byte byte2) 
+double ConvertSpecialTwosComplement(byte byte1, byte byte2) 
 {
-    return 0;
+    double[] CONVERSION_TABLE = { -128, 64, 32, 16, 8, 4, 2, 1, 0.5 };
+
+    double result = 0;
+
+    Convert.ToString(byte1);
+
+    short[] bits = new short[9];
+
+    {
+        short[] byte1Bits = GetBitArray(byte1);
+        short[] byte2Bits = GetBitArray(byte2);
+
+        for(int i = 0; i < 8; i++)
+        {
+            bits[i] = byte1Bits[i];
+        }
+        bits[8] = byte2Bits[0];
+    }
+
+    for(int i = 0; i < 9; i++)
+    {
+        result += bits[i] * CONVERSION_TABLE[i];
+    }
+
+    return result;
+}
+
+short[] GetBitArray(byte b)
+{
+    bool[] bitArrayBool = new bool[8];
+    for (int i = 0; i < 8; i++)
+    {
+        bitArrayBool[7 - i] = (b & (1 << i)) != 0; // Check if the i-th bit is set
+    }
+
+    short[] bitArrayShort = new short[8];
+    for(int i = 0; i < 8; i++)
+    {
+        if(bitArrayBool[i])
+        {
+            bitArrayShort[i] = 1;
+            continue;
+        }
+        bitArrayShort[i] = 0;
+    }
+
+    return bitArrayShort;
 }
