6004b439358bbe3644a5b283641aefe3edb5321c
Jonas <sturmente@proton.me>
2024-05-06 17:50:13 +0200

    Added description and created function skeleton

--- specialTwosComplementConverter/Program.cs
+++ specialTwosComplementConverter/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+// Convert two bytes with the following format into a float
+// byte 1 -128 64 32 16 8 4 2 1
+// byte 2 0.5 x x x x x x x
+// The bytes are read from a LM75 module 
+float convertSpecialTwosComplement(byte byte1, byte byte2) 
+{
+    return 0;
+}
